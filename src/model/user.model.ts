1a10007beffed9a4f04df1aa89b9a7b4498c7ae6
harshadbirajdar <hbirajdar1@gmail.com>
2023-08-09 09:44:50 +0530

    add: index added in user model :memo:
    Signed-off-by: harshadbirajdar <hbirajdar1@gmail.com>
    
    On branch harshad
    Your branch is up to date with 'origin/harshad'.
    
    Changes to be committed:
    	modified:   src/model/user.model.ts

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -73,5 +73,7 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
+userSchema.index({ email: 1 });
+
 const UserModel = model<IUser, IuserMethod>("User", userSchema);
 export default UserModel;

f98920e308484bba4b6afc11e813ac0476554f4e
Harshad Birajdar <hbirajdar1@gmail.com>
2024-06-20 22:27:05 +0530

    feat: Implement Node.js clustering to enhance application performance
    Signed-off-by: Harshad Birajdar <hbirajdar1@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,30 @@
+import cluster from "cluster";
+import os from "os";
+
 import app from "./app";
 import config from "./config";
-import logger from "./logger";
 import "./config/db";
+import logger from "./logger";
 
-app.listen(config.PORT, (): void => {
-  logger.info(`Server started successfully on port ${config.PORT}`);
-});
+const numCPUs = config.env === "production" ? os.cpus().length : 1;
+
+if (cluster.isPrimary) {
+  logger.info(`Master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    logger.info(`Worker ${worker.process.pid} died`);
+    logger.info("Starting a new worker");
+    cluster.fork();
+  });
+} else {
+  app.listen(config.PORT, (): void => {
+    logger.info(
+      `Worker ${process.pid} started and Server started successfully on port ${config.PORT}`
+    );
+  });
+}
